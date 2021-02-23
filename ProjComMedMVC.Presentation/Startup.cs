708dee21675909586cbcd74a96eb0fcb08c7afdc
Marco Pacheco <79469633+marcopacheco-dev@users.noreply.github.com>
2021-02-23 18:59:34 -0300

    03 - Construindo padrão MVC

--- ProjComMedMVC.Presentation/Startup.cs
+++ ProjComMedMVC.Presentation/Startup.cs
@@ -16,6 +16,7 @@ namespace ProjComMedMVC.Presentation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,18 @@ namespace ProjComMedMVC.Presentation
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseStaticFiles();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app.UseRouting();
+            
+            app.UseEndpoints(
+                endpoints =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}"
+                        );
                 });
-            });
         }
     }
 }
