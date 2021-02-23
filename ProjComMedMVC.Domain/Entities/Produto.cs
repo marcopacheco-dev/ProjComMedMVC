5e90700ff760fb3b65febce3d3ff18cf1be60928
Marco Pacheco <79469633+marcopacheco-dev@users.noreply.github.com>
2021-02-23 12:15:55 -0300

    01 - escopo
    Propriedades e relacionamentos criados.

---
+++ ProjComMedMVC.Domain/Entities/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjComMedMVC.Domain.Entities
+{
+   public class Produto
+    {
+        #region Propriedades
+        public Guid IdProduto { get; set; }
+        public string Nome  { get; set; }
+        public string Quantidade { get; set; }
+        public string Preco { get; set; }
+        public Guid IdComprador { get; set; }
+        public Guid IdEmpresa { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+        Empresa empresa { get; set; }
+        Comprador comprador { get; set; }
+
+        #endregion
+    }
+}
