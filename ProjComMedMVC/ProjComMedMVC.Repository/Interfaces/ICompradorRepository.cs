51c880e768dfd1aa1a6e907ddb1b1d9189cc3ecf
Marco Pacheco <79469633+marcopacheco-dev@users.noreply.github.com>
2021-02-27 18:02:57 -0300

    SGC - Medvitalis

---
+++ ProjComMedMVC/ProjComMedMVC.Repository/Interfaces/ICompradorRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjComMedMVC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjComMedMVC.Repository.Interfaces
+{
+   public interface ICompradorRepository
+        : IBaseRepository<Comprador>
+    {
+        Comprador ObterPorCpf(string Cpf);
+        
+    }
+}
